64785bf4b38bb8ecd6c721f3e5e8db96c1e64ff6
Sam Leevoid <samleevoid@gmail.com>
2021-10-21 15:37:03 +0200

    Update carcassa restServer

---
+++ restServer/helpers/db-validators.js
@@ -0,0 +1,13 @@
+const Usuari = require("../models/usuari");
+
+const emailExisteix = async (correu = "") => {
+  // Verificar si el correu ja existeix a la db
+  const existeix = await Usuari.findOne({ correu });
+  if (existeix) {
+    throw new Error(`El correu ${correu} ja està registrat`);
+  }
+};
+
+module.exports = {
+  emailExisteix,
+};
--- restServer/routes/usuaris.js
+++ restServer/routes/usuaris.js
@@ -5,6 +5,8 @@ const { check, validationResult } = require("express-validator");
 
 const { usuarisGet, usuarisPost } = require("../controllers/usuaris");
 
+const { emailExisteix } = require("../helpers/db-validators");
+
 const router = Router();
 
 const validarCamps = (req, res, next) => {
@@ -20,12 +22,11 @@ router.get("/", (req = request, res = response) => {
 });
 
 router.get("/usuaris", usuarisGet);
-
 router.post(
   "/usuaris",
   [
     check("correu", "El correu no és vàlid").isEmail(),
-    // check("correu").custom(emailExisteix)
+    check("correu").custom(emailExisteix),
     check("nom", "El nom és obligatori").not().isEmpty(),
     check("password", "El password ha de tenir més de 6 caràcters").isLength({
       min: 6,
@@ -35,5 +36,8 @@ router.post(
   ],
   usuarisPost
 );
+
+// router.put /:id //update d'un determinat usuari
+// router.delete /:id //eliminar un usuari determinat
 
 module.exports = router;
